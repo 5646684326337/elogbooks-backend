289b1de962188bb78205657e175259a8d011ca4b
Matt Johnson <3819johnson@gmail.com>
2018-06-18 18:55:55 +0100

    Added email validation as per Symfony/Doctrine docs

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * User
@@ -92,6 +94,17 @@ class User
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * @param ClassMetadata $metadata
+     */
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('email', new Assert\Email(array(
+            'message' => 'The email "{{ value }}" is not a valid email.',
+            'checkMX' => true,
+        )));
     }
 }
 
